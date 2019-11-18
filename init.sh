94e43c2d3939e1c06edc709d3df9e57988cce573
chaudhur <chaudhur@enst.fr>
2019-11-18 16:04:02 +0100

    changed init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #module() { eval `/usr/bin/modulecmd bash $*`; }
 #export -f module
-MODULEPATH=$MODULEPATH:./:./modules
-export MODULEPATH
+#MODULEPATH=$MODULEPATH:./:./modules
+#export MODULEPATH
+export PATH=$PATH:/comelec/softs/opt/ARM_tools/Mali_Graphics_Debugger_v4.4.1.0271762a_Linux_x64/gui/
+export PATH=$PATH:/comelec/softs/opt/ARM_tools/Mali_Offline_Compiler_v6.2.0
+export PATH=$PATH:/comelec/softs/opt/ARM_tools/ds5-ce/bin
